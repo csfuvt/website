ed6c88a7c1e45c93c35485a6b2f4fa35f6a4b06b
Diana-Nicoleta Sirbu <118627418+Hbax112@users.noreply.github.com>
2024-05-14 02:41:23 +0300

    Dialog Francof Volume + Logo + Titlu (#11)

---
+++ src/routes/-components/KVolumeCard/KVolumeCard.tsx
@@ -0,0 +1,28 @@
+import styles from './KVolumeCard.module.css';
+import volumeImage from '../../../../public/copertavolum.png';
+
+export const KVolumeCard = ({
+  title,
+  issueNumber,
+  buttonText,
+}: {
+  title: string;
+  issueNumber: string;
+  buttonText: string;
+}) => {
+  return (
+    <div className={styles.card}>
+      <div
+        className={styles.backgroundImage}
+        style={{ backgroundImage: `url(${volumeImage})` }}>
+        <h3 className={styles.title}>{title}</h3> {}
+      </div>
+      <div className={styles.content}>
+        <p className={styles.issueNumber}>{issueNumber}</p>
+        <button className={styles.button}>{buttonText}</button>
+      </div>
+    </div>
+  );
+};
+
+export default KVolumeCard;
--- src/routes/research_/publications_/dialogue-francophones_/volumes/index.tsx
+++ src/routes/research_/publications_/dialogue-francophones_/volumes/index.tsx
@@ -1,9 +1,31 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { KBanner } from '../../../../-components/KBanner/KBanner';
+import { KVolumeCard } from '../../../../-components/KVolumeCard/KVolumeCard';
+import styles from './VolumePage.module.css';
+
+const VolumePage = () => {
+  return (
+    <div>
+      <KBanner label="Dialogues Francophones - VOLUME" />
+
+      <div className={styles.cardsContainer}>
+        {Array.from({ length: 8 }).map((_, index) => (
+          <KVolumeCard
+            key={index}
+            title="De la phrase / énoncé au texte / discours."
+            issueNumber="NO 26-67/2022-2023"
+            buttonText="Deschide >"
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export const Route = createFileRoute(
   '/research/publications/dialogue-francophones/volumes/'
 )({
-  component: () => (
-    <div>Hello /research/publications/dialogue-francophones/volumes/!</div>
-  ),
+  component: VolumePage,
 });
+
+export default VolumePage;
