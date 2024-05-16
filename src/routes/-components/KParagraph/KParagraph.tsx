1e5fef8ff5ac702cd8f484b89bda2703f2bd2c8a
gabrielabutnaru <gabrielabutnaru24@gmail.com>
2024-05-16 22:48:00 +0300

    Implement Committees Page

--- src/routes/-components/KParagraph/KParagraph.tsx
+++ src/routes/-components/KParagraph/KParagraph.tsx
@@ -1,15 +1,21 @@
 import './styles.css';
+import { KSubtitle } from '../KSubtitle/KSubtitle.tsx';
 export const KParagraph = ({
   text,
   list,
   listElements,
+  title,
+  titleLabel,
 }: {
   text: string;
   list?: boolean;
   listElements?: string[];
+  title?: boolean;
+  titleLabel?: string;
 }) => {
   return (
     <div className="line">
+      {title && <KSubtitle label={titleLabel} />}
       <span className="lineP">{text}</span>
       {list && (
         <ul>
