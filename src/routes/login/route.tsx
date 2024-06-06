076121dfc54579e9a213a29833a454e3a5c876e5
Gabriela Butnaru <104324524+gabrielabutnaru@users.noreply.github.com>
2024-06-06 22:08:09 +0300

    Implement login login using the login endpoint (#39)

--- src/routes/login/route.tsx
+++ src/routes/login/route.tsx
@@ -1,29 +1,90 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect, useNavigate } from '@tanstack/react-router';
 import styles from './LogInPage.module.css';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import axios from 'axios';
+import { md5 } from 'js-md5';
+import { useAuth } from '../../hooks/useAuth.ts';
 
-const LogInPage = () => {
+export const Route = createFileRoute('/login')({
+  beforeLoad: ({ context }) => {
+    if (context.isLoggedIn) {
+      throw redirect({
+        to: '/',
+      });
+    }
+  },
+  component: LogInPage,
+});
+
+interface LogIn {
+  email: string;
+  password: string;
+}
+
+function LogInPage() {
+  const { signIn } = useAuth();
+  const navigate = useNavigate();
+
+  const { handleSubmit, control } = useForm<LogIn>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<LogIn> = data => {
+    axios
+      .post('/auth/login', {
+        email: data.email,
+        password: md5(data.password),
+      })
+      .then(({ data: { access_token } }) => {
+        signIn(access_token);
+        navigate({ to: '/' });
+      });
+  };
+
   return (
     <div className={styles.loginContainer}>
       <div className={styles.loginForm}>
         <h1>Log In</h1>
         <form>
-          <input type="email" placeholder="E-mail" required />
-          <input type="password" placeholder="Password" required />
-          <a href="#forgot" className={styles.forgotPassword}>
-            Forgot your password?
-          </a>
-          <button type="submit">Log In</button>
-          <div className={styles.signupLink}>
-            Don’t have an account yet? <a href="#signup">Create an account</a>
-          </div>
+          <Controller
+            name="email"
+            defaultValue=""
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <input
+                value={value}
+                onChange={onChange}
+                type="email"
+                placeholder="E-mail"
+                required
+              />
+            )}
+          />
+          <Controller
+            name="password"
+            defaultValue=""
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <input
+                value={value}
+                onChange={onChange}
+                type="password"
+                placeholder="Password"
+                required
+              />
+            )}
+          />
+          {/*<a href="#forgot" className={styles.forgotPassword}>*/}
+          {/*  Forgot your password?*/}
+          {/*</a>*/}
+          <button type="submit" onClick={handleSubmit(onSubmit)}>
+            Log In
+          </button>
         </form>
       </div>
     </div>
   );
-};
-
-export const Route = createFileRoute('/login')({
-  component: LogInPage,
-});
-
-export default LogInPage;
+}
