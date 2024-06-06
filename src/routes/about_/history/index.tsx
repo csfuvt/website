18c23960af7e0a514d53a12c8317f9c5e28c203b
didu <diana.sirbu03@e-uvt.ro>
2024-06-07 00:26:27 +0300

    sper sa nu fie problemleme

--- src/routes/about_/history/index.tsx
+++ src/routes/about_/history/index.tsx
@@ -4,11 +4,13 @@ import { KBanner } from '../../-components/KBanner/KBanner';
 import istoric1 from '../../../../public/Despre_noi/istoric1.jpg';
 import istoric2 from '../../../../public/Despre_noi/istoric2.jpg';
 import istoric3 from '../../../../public/Despre_noi/istoric3.jpg';
+import { useTranslation } from 'react-i18next';
 
 const HistoryPage = () => {
+  const { t } = useTranslation();
   return (
     <div>
-      <KBanner label="ISTORIC" />
+      <KBanner label={t('ISTORIC')} />
       <div className={styles.pageContainer}>
         <div className={styles.sectionContainer}>
           <img
@@ -17,30 +19,20 @@ const HistoryPage = () => {
             className={styles.imageBox}
           />
           <div className={styles.textContainer}>
-            <div className={styles.title}>Istoric</div>
+            <div className={styles.title}>{t('Istoric')}</div>
             <div className={styles.paragraph}>
               <div className={styles.line}>
                 <p>
-                  Centrul de Studii Francofone, având drept obiect cercetarea în
-                  domeniul francofoniei, a fost creat în 1994 la iniţiativa
-                  Prof. univ. dr. Margareta Gyurcsik şi reuneşte cercetători şi
-                  cadre didactice din Colectivul de Limba franceză de la
-                  Facultatea de Litere, Istorie şi Teologie, Universitatea de
-                  Vest din Timişoara.
+                  {t(
+                    'Centrul de Studii Francofone, având drept obiect cercetarea în domeniul francofoniei, a fost creat în 1994 la iniţiativa Prof. univ. dr. Margareta Gyurcsik şi reuneşte cercetători şi cadre didactice din Colectivul de Limba franceză de la Facultatea de Litere, Istorie şi Teologie, Universitatea de Vest din Timişoara.'
+                  )}
                 </p>
               </div>
               <div className={styles.line}>
                 <p>
-                  Domeniul francofoniei este înţeles în sens larg prin
-                  explorarea literaturilor din arii geografice diverse (Canada,
-                  Maghreb, Africa subsahariană etc.), incluzând literatura
-                  franceză contemporană. De asemenea, programele de cercetare şi
-                  publicaţiile Centrului acordă constant atenţie contribuţiilor
-                  româneşti: literatura scriitorilor români francofoni,
-                  traducerea literară din şi în limba franceză, autotraducerea,
-                  precum şi aportul cercetării româneşti la exegeza şi
-                  istoriografia în curs de constituire a literaturilor
-                  francofone.
+                  {t(
+                    ' Domeniul francofoniei este înţeles în sens larg prin explorarea literaturilor din arii geografice diverse (Canada, Maghreb, Africa subsahariană etc.), incluzând literatura franceză contemporană. De asemenea, programele de cercetare şi publicaţiile Centrului acordă constant atenţie contribuţiilor româneşti: literatura scriitorilor români francofoni, traducerea literară din şi în limba franceză, autotraducerea, precum şi aportul cercetării româneşti la exegeza şi istoriografia în curs de constituire a literaturilor francofone.'
+                  )}
                 </p>
               </div>
             </div>
@@ -52,18 +44,16 @@ const HistoryPage = () => {
             <div className={styles.paragraph}>
               <div className={styles.lineBlue}>
                 <p>
-                  Centrul a avut şi are un rol federator, propunând teme de
-                  investigaţie şi puncte de joncţiune pentru cercetări diverse,
-                  care pun în valoare specializări ştiinţifice şi opţiuni
-                  metodologice variate.
+                  {t(
+                    'Centrul a avut şi are un rol federator, propunând teme de investigaţie şi puncte de joncţiune pentru cercetări diverse, care pun în valoare specializări ştiinţifice şi opţiuni metodologice variate.'
+                  )}
                 </p>
               </div>
               <div className={styles.lineBlue}>
                 <p>
-                  Centrul contribuie substanţial la configurarea şi derularea
-                  programelor de studiu ale Catedrei de Limbi Romanice prin
-                  includerea domeniului literar francofon atât în ciclul de
-                  licenţă, cât mai ales în cele de masterat şi doctorat.
+                  {t(
+                    'Centrul contribuie substanţial la configurarea şi derularea programelor de studiu ale Catedrei de Limbi Romanice prin includerea domeniului literar francofon atât în ciclul de licenţă, cât mai ales în cele de masterat şi doctorat.'
+                  )}
                 </p>
               </div>
             </div>
@@ -85,25 +75,23 @@ const HistoryPage = () => {
             <div className={styles.paragraph}>
               <div className={styles.line}>
                 <p>
-                  Din 1995, Centrul publică revista de literatură francofonă
-                  contemporană Dialogues francophones. Începând cu anul 2009,
-                  numerele au devenit tematice.
+                  {t(
+                    'Din 1995, Centrul publică revista de literatură francofonă contemporană Dialogues francophones. Începând cu anul 2009, numerele au devenit tematice.'
+                  )}
                 </p>
               </div>
               <div className={styles.line}>
                 <p>
-                  Din 2004, Centrul organizează anual un colocviu de studii
-                  francofone, intitulat iniţial Contributions roumaines à la
-                  francophonie, apoi, din 2010, Colloque International d’Études
-                  Francophones à Timişoara (CIEFT). Lucrările sunt publicate în
-                  volume intitulate Agapes francophones.
+                  {t(
+                    'Din 2004, Centrul organizează anual un colocviu de studii francofone, intitulat iniţial Contributions roumaines à la francophonie, apoi, din 2010, Colloque International d’Études Francophones à Timişoara (CIEFT). Lucrările sunt publicate în volume intitulate Agapes francophones.'
+                  )}
                 </p>
               </div>
               <div className={styles.line}>
                 <p>
-                  Centrul a iniţiat o serie de proiecte, în domenii variate:
-                  studii literare, traducere, lingvistică, didactica limbii
-                  franceze ca limbă străină.
+                  {t(
+                    'Centrul a iniţiat o serie de proiecte, în domenii variate: studii literare, traducere, lingvistică, didactica limbii franceze ca limbă străină.'
+                  )}
                 </p>
               </div>
             </div>
