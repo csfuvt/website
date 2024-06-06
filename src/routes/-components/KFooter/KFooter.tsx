efafdbb3ee6bccc97a629417d4db3a660f1c9f5c
didu <diana.sirbu03@e-uvt.ro>
2024-06-06 22:42:41 +0300

    pg home+pg despre noi+contat

--- src/routes/-components/KFooter/KFooter.tsx
+++ src/routes/-components/KFooter/KFooter.tsx
@@ -3,8 +3,10 @@ import logo from '../../../assets/logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCompass, faEnvelope } from '@fortawesome/free-regular-svg-icons';
 //import LogInPage from '../../login/route';
+import { useTranslation } from 'react-i18next';
 
 export const KFooter = () => {
+  const { t } = useTranslation();
   return (
     <>
       <div className="footer">
@@ -25,15 +27,15 @@ export const KFooter = () => {
           <div className="row">
             <FontAwesomeIcon icon={faCompass} />
             <div>
-              <div>Vasile Pârvan blvd. no. 4</div>
-              <div>Timişoara Timiş</div>
-              <div>România 300223</div>
+              <div>{t('Vasile Pârvan blvd. no. 4')}</div>
+              <div>{t('Timişoara Timiş')}</div>
+              <div>{t('România 300223')}</div>
             </div>
           </div>
         </div>
       </div>
       <div className="stamp">
-        Jurnal francofon de literatură contemporană
+        {t('Jurnal francofon de literatură contemporană')}
         <a href="/login" className="loginButton">
           Log In
         </a>{' '}
