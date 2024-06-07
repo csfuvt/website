d01ceb312c0f1f013bed36bac8f93aa83348bbef
gabrielabutnaru <gabrielabutnaru24@gmail.com>
2024-06-07 04:24:15 +0300

    Integrate translations delete endpoint

--- src/routes/-components/KTranslationCard/KTranslationCard.tsx
+++ src/routes/-components/KTranslationCard/KTranslationCard.tsx
@@ -1,25 +1,62 @@
-import styles from './KTranslationCard.module.css';
+import './styles.css';
+import { DeleteOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useAuth } from '../../../hooks/useAuth.ts';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const deleteTranslation = (id: number) =>
+  axios.delete(`/translations/${id}`).then(res => res.data);
 
 export const KTranslationCard = ({
+  id,
   summaryText,
   link,
   translationImage,
 }: {
+  id: number;
   summaryText: string;
   link?: string;
   translationImage: string;
 }) => {
+  const { isLoggedIn } = useAuth();
+  const queryClient = useQueryClient();
+  const { mutate, isPending } = useMutation({
+    mutationFn: deleteTranslation,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['translations'] });
+      toast.success('Traducerea a fost ștearsă cu succes');
+    },
+    onError: () => toast.error('A apărut o eroare în momentul ștergerii'),
+  });
+
   return (
-    <div className={styles.card}>
-      <div
-        className={styles.backgroundImage}
-        style={{ backgroundImage: `url(${translationImage})` }}></div>
-      <div className={styles.detailsOverlay}>
-        <div className={styles.content}>
-          <a href={link} className={styles.summaryText}>
-            {summaryText}
-          </a>
+    <div className="container">
+      <img
+        src={translationImage}
+        className="image"
+        onClick={() => {
+          if (link) {
+            window.location.href = link;
+          }
+        }}
+      />
+      {isLoggedIn && (
+        <div className="delete">
+          <Button
+            type="primary"
+            icon={<DeleteOutlined />}
+            shape="circle"
+            onClick={() => {
+              mutate(id);
+            }}
+            loading={isPending}
+          />
         </div>
+      )}
+      <div className="middle">
+        <div>{summaryText}</div>
       </div>
     </div>
   );
