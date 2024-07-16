b9fc0e6ed319d4513aa97686a9239ceff2c82195
didu <diana.sirbu03@e-uvt.ro>
2024-07-17 02:08:40 +0300

    KBand, KBanner, KFooter, KMemberCard, MembersPage, Contact

--- src/routes/-components/KBanner/KBanner.tsx
+++ src/routes/-components/KBanner/KBanner.tsx
@@ -1,13 +1,18 @@
+import React from 'react';
 import styles from './KBanner.module.css';
 import { KBand } from '../KBand/KBand.tsx';
 
-export const KBanner = ({ label }: { label: string }) => {
+interface KBannerProps {
+  label: string;
+}
+
+export const KBanner: React.FC<KBannerProps> = ({ label }) => {
   return (
-    <>
+    <div className={styles.bannerContainer}>
       <div className={styles.gradientSection}>
         <h2>{label}</h2>
       </div>
       <KBand />
-    </>
+    </div>
   );
 };
