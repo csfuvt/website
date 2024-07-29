4db04bb39cf1fb62bcd4ff198551cdc16fb755ae
Alexandru Nagy <147058886+alexutzzro@users.noreply.github.com>
2024-07-30 02:18:15 +0300

    I added new banners on carousel first page (with Alesia) (#57)

--- src/routes/-components/KMovingBanner/KMovingBanner.tsx
+++ src/routes/-components/KMovingBanner/KMovingBanner.tsx
@@ -5,6 +5,10 @@ import styles from './KMovingBanner.module.css';
 import image1 from '../../../../public/MovingBanner1.png';
 import image2 from '../../../../public/MovingBanner2.png';
 import image3 from '../../../../public/MovingBanner3.png';
+import image4 from '../../../../public/MovingBanner4.png';
+import image5 from '../../../../public/MovingBanner5.png';
+import image6 from '../../../../public/MovingBanner6.png';
+import image7 from '../../../../public/MovingBanner7.png';
 
 export const KMovingBanner: React.FC = () => {
   return (
@@ -32,6 +36,26 @@ export const KMovingBanner: React.FC = () => {
           <div
             className={styles.carouselItem}
             style={{ backgroundImage: `url(${image3})` }} />
+        </div>
+        <div>
+          <div
+            className={styles.carouselItem}
+            style={{ backgroundImage: `url(${image4})` }} />
+        </div>
+        <div>
+          <div
+            className={styles.carouselItem}
+            style={{ backgroundImage: `url(${image5})` }} />
+        </div>
+        <div>
+          <div
+            className={styles.carouselItem}
+            style={{ backgroundImage: `url(${image6})` }} />
+        </div>
+        <div>
+          <div
+            className={styles.carouselItem}
+            style={{ backgroundImage: `url(${image7})` }} />
         </div>
       </Carousel>
     </ConfigProvider>
