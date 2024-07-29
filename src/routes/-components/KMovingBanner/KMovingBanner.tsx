7b7457b277f8c0cf1cf3a1aa9ad276aa5f42bcdc
Alexandru Nagy <147058886+alexutzzro@users.noreply.github.com>
2024-07-30 01:29:00 +0300

    Revert "I created 404 page with French locale and I added next and prev arrow…"
    This reverts commit bc1dcf022fdb9a72c083d1b75fd3e816c7a773d1.

--- src/routes/-components/KMovingBanner/KMovingBanner.tsx
+++ src/routes/-components/KMovingBanner/KMovingBanner.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Carousel, ConfigProvider } from 'antd';
+import { Carousel } from 'antd';
 import styles from './KMovingBanner.module.css';
 
 import image1 from '../../../../public/MovingBanner1.png';
@@ -8,33 +8,26 @@ import image3 from '../../../../public/MovingBanner3.png';
 
 export const KMovingBanner: React.FC = () => {
   return (
-    <ConfigProvider
-      theme={{
-        components: {
-          Carousel: {
-            arrowSize: 30
-          }
-        }
-      }}
-    >
-    <Carousel autoplay arrows infinite={true}>
-        <div>
-          <div
-            className={styles.carouselItem}
-            style={{ backgroundImage: `url(${image1})` }} />
-        </div>
-        <div>
-          <div
-            className={styles.carouselItem}
-            style={{ backgroundImage: `url(${image2})` }} />
-        </div>
-        <div>
-          <div
-            className={styles.carouselItem}
-            style={{ backgroundImage: `url(${image3})` }} />
-        </div>
-      </Carousel>
-    </ConfigProvider>
+    <Carousel autoplay>
+      <div>
+        <div
+          className={styles.carouselItem}
+          style={{ backgroundImage: `url(${image1})` }}
+        />
+      </div>
+      <div>
+        <div
+          className={styles.carouselItem}
+          style={{ backgroundImage: `url(${image2})` }}
+        />
+      </div>
+      <div>
+        <div
+          className={styles.carouselItem}
+          style={{ backgroundImage: `url(${image3})` }}
+        />
+      </div>
+    </Carousel>
   );
 };
 
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,7 +2,6 @@ import { createRootRouteWithContext, Outlet } from '@tanstack/react-router';
 import { KFooter } from './-components/KFooter/KFooter.tsx';
 import { KHeader } from './-components/KHeader/KHeader.tsx';
 import { AuthContextProps } from '../auth.context.tsx';
-import { useTranslation } from 'react-i18next';
 
 export const Route = createRootRouteWithContext<AuthContextProps>()({
   component: () => (
@@ -13,15 +12,4 @@ export const Route = createRootRouteWithContext<AuthContextProps>()({
       {/*<TanStackRouterDevtools />*/}
     </>
   ),
-  notFoundComponent: () => {
-    const { t } = useTranslation();
-    return (
-      <center>
-      <div>
-        <h1>{t('404 - Pagina nu a fost găsită')}</h1>
-        <p>{t('Scuze, dar nu am găsit pagina căutată de tine.')}</p>
-      </div>
-    </center>
-    );
-  },
 });
