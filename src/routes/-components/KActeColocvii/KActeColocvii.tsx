0196d96dc6cf460ac52acd17f320b7163c8ad003
Iasminaaaaa <iasmina.gusca23@gmail.com>
2024-12-02 00:28:11 +0200

    Implement-Page-Actele-Colocviilor-Franco-Romane-De-Lingvistica (#100)

--- src/routes/-components/KActeColocvii/KActeColocvii.tsx
+++ src/routes/-components/KActeColocvii/KActeColocvii.tsx
@@ -1,22 +1,38 @@
-import './styles.css';
+import styles from './KActeColocvii.module.css';
+
 export const KActeColocvii = ({
+  title,
   summaryText,
-  link,
+  Sommairelink,
+  APUlink,
+  TexteIntegrallink,
   bookImage,
 }: {
+  title: string;
   summaryText: string;
-  link: string;
+  Sommairelink: string;
+  APUlink?: string;
+  TexteIntegrallink?: string;
   bookImage: string;
 }) => {
   return (
-    <a href={link} className="cardContainer">
-      <div className="card">
-        <div
-          className="backgroundImage"
-          style={{ backgroundImage: `url(${bookImage})` }}></div>
-        <div>{summaryText}</div>
+    <div className={styles.acteColocviiContainer}>
+      <div className={styles.card}>
+        {}
+        <img className={styles.cardImage} src={bookImage} alt={title} />
+
+        <div className={styles.cardContent}>
+          {}
+          <div className={styles.title}>{title}</div>
+          <div className={styles.summaryText}>{summaryText}</div>
+          <div className={styles.colocviiDetails}>
+            <a href={Sommairelink}><strong>Sommaire</strong></a>
+            {APUlink && <a href={APUlink}><strong>APU</strong></a>}
+            {TexteIntegrallink && <a href={TexteIntegrallink}><strong>Texte intégral</strong></a>}
+          </div>
+        </div>
       </div>
-    </a>
+    </div>
   );
 };
 
